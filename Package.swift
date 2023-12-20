2e5f189f6bb9b1e397d461389dc925fa470aa66b
Joe Antonetti <antwwwon@icloud.com>
2023-12-19 21:40:15 -0500

    Added SheetButton

--- Package.swift
+++ Package.swift
@@ -1,23 +1,17 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SheetButton",
+    platforms: [.iOS(.v15), .macOS(.v13), .watchOS(.v7), .macCatalyst(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SheetButton",
             targets: ["SheetButton"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SheetButton"),
-        .testTarget(
-            name: "SheetButtonTests",
-            dependencies: ["SheetButton"]),
+            name: "SheetButton")
     ]
 )
--- Sources/SheetButton/SheetButton.swift
+++ Sources/SheetButton/SheetButton.swift
@@ -1,2 +1,40 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  SheetButton.swift
+//
+//
+//  Created by Joseph Antonetti on 12/19/23.
+//
+
+import SwiftUI
+
+public struct SheetButton<Label : View, Sheet : View>: View {
+    
+    @ViewBuilder let sheet : () -> Sheet
+    @ViewBuilder let label : () -> Label
+    
+    @State private var isPresented = false
+    
+    public init(
+        sheet: @escaping () -> Sheet,
+        label: @escaping () -> Label) {
+        self.sheet = sheet
+        self.label = label
+    }
+    
+    public var body: some View {
+        Button {
+            isPresented = true
+        } label: {
+            label()
+        }
+        .sheet(isPresented: $isPresented, content: sheet)
+    }
+}
+
+#Preview {
+    SheetButton {
+        Text("Sheet")
+    } label: {
+        Text("Label")
+    }
+}
